5ed770f1e76524c819183d531921ac2050584450
Mark Harding <mark@kramnorth.com>
2015-05-19 14:48:39 +0100

    updates site to include new bg

---
+++ dist/scripts/controllers/code.js
@@ -0,0 +1,1 @@
+define(["angular"],function(angular){"use strict";angular.module("orgApp.controllers.CodeCtrl",[]).controller("CodeCtrl",["$scope",function($scope){$scope.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}])});
